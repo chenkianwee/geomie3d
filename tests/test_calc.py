274bef5d1e0052b41b053539a290b0af1c66d3c6
chenkianwee <chenkianwee@gmail.com>
2024-02-29 13:39:38 -0500

    add new test2

--- tests/test_calc.py
+++ tests/test_calc.py
@@ -1,6 +1,6 @@
 import geomie3d
 
-def are_bbox1_related_bbox2():
+def test_are_bbox1_related_bbox2():
     bbox1 = geomie3d.utility.Bbox([1,1,0,10,10,5])
     bbox2 = geomie3d.utility.Bbox([1,1,0,10,10,5])
 
@@ -11,10 +11,8 @@ def are_bbox1_related_bbox2():
     bbox6 = geomie3d.utility.Bbox([15,19,11,21,23,14])
 
     are_related = geomie3d.calculate.are_bboxes1_related2_bboxes2([bbox1, bbox2, bbox5], [bbox3, bbox4, bbox6])
-    print(are_related)
 
-def bbox_center():
+def test_bbox_center():
     bbox1 = geomie3d.create.bbox_frm_arr([1,1,0,10,10,5])
     bbox2 = geomie3d.create.bbox_frm_arr([2,2,1,8,8,4])
     center_pts = geomie3d.calculate.bboxes_centre([bbox1, bbox2])
-    print(center_pts)
---
+++ tests/test_viz.py
@@ -0,0 +1,38 @@
+import geomie3d
+
+def test_viz_topo():
+    #defining the boundary wire
+    xyz_list1 = [(10,10,0), (20,10,0), (20,20,0)]
+    att_list1 = [{"type":"bus stop"}, {"type":"shop"}, {"type":"monument"}]
+    vlist1 = geomie3d.create.vertex_list(xyz_list1, attributes_list = att_list1)
+    pl_att1 = {"id":0, "type": "sidewalk"}
+    pline_edge1 = geomie3d.create.pline_edge_frm_verts(vlist1, attributes = pl_att1)
+
+    xyz_list2 = [(20,20,0), (10,20,0), (10,10,0)]
+    att_list2 = [{"type":{"test":1}}, {"type":"shop"}, {"type":"bus stop", "id":100}]
+    vlist2 = geomie3d.create.vertex_list(xyz_list2, attributes_list = att_list2)
+    pl_att2 = {"id":1, "type": "road"}
+    pline_edge2 = geomie3d.create.pline_edge_frm_verts(vlist2, attributes = pl_att2)
+
+    bdry_edge_list = [pline_edge1, pline_edge2]
+    w_att1 = {"id":0, "type": "plot"}
+    bdry_wire = geomie3d.create.wire_frm_edges(bdry_edge_list, attributes = w_att1)
+
+    #defining the hole wire
+    xyz_list3 = [(12,12,0), (18,12,0), (18,18,0)]
+    vlist3 = geomie3d.create.vertex_list(xyz_list3)
+    pline_edge3 = geomie3d.create.pline_edge_frm_verts(vlist3)
+
+    xyz_list4 = [(18,18,0), (12,18,0), (12,12,0)]
+    vlist4 = geomie3d.create.vertex_list(xyz_list4)
+    pline_edge4 = geomie3d.create.pline_edge_frm_verts(vlist4)
+
+    hole_edge_list = [pline_edge3, pline_edge4]
+    hole_wire = geomie3d.create.wire_frm_edges(hole_edge_list)
+
+    #create the face
+    f_att = {"type": "residential", "id": 0}
+    face = geomie3d.create.polygon_face_frm_wires(bdry_wire, [hole_wire], attributes = f_att)
+
+    viz_dict = {"topo_list":[face], "colour": [0,1,0,1], "attributes": []}
+    geomie3d.viz.viz([viz_dict], gl_option='opaque')
